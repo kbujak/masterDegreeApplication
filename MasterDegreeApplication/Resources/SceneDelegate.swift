36bbc34b2bd4ae40db71979a0c4d34ae7f442e60
Krystian Bujak <Booyac@MacBook-Pro-macbook.local>
2020-03-19 22:18:43 +0100

    VC is now visible

---
+++ MasterDegreeApplication/Resources/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  MasterDegreeApplication
+//
+//  Created by Krystian Bujak on 19/03/2020.
+//  Copyright © 2020 Booyac IT. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let VC = SignInViewController()
+            window.rootViewController = VC
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
+
